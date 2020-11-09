a9880edfdde820f4d91b77cdc13702eec6c7fd85
alessio <alessio.cirillo.1995@gmail.com>
2020-11-09 16:09:44 +0100

    prev and next arrow

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+const app = new Vue({
+
+    el: '#app',
+
+    data:{
+        indexImages: 0,
+        images: [
+            './img/image1.jpg',
+            './img/image2.jpg',
+            './img/image3.jpg',
+            './img/image4.jpg'
+        ]
+    },
+    methods:{
+        nextPhoto(){
+            this.indexImages += 1;
+
+            if (this.indexImages > (this.images.length - 1) ){
+                this.indexImages = 0;
+            }
+        },
+
+        prevPhoto(){
+            this.indexImages -= 1;
+
+            if (this.indexImages < 0){
+                this.indexImages = this.images.length - 1;
+            }
+        }
+    }
+});
